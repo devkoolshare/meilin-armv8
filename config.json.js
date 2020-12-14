1ae53b06e2ae8840a7de9590abaf708acd11ba8f
zhangfei <1163167506@qq.com>
2020-12-14 17:30:13 +0800

    modify md5

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "0600667B59551483EC075BF6D5600204", 
+    "md5": "0600667b59551483ec075bf6d5600204", 
     "module": "xunyou", 
     "title": "迅游加速器", 
     "version": "1.0.1.2"
